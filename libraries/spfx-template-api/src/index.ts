e21e9f601a857a1191c248a9e4783f336008b96e
Nick Pape <5674316+nick-pape@users.noreply.github.com>
2025-10-07 17:02:52 -0500

    Initial checkin of 3 projects: a build rig, an empty scaffolding API, and an empty CLI. Also updates READMEs, licenses, etc.

---
+++ libraries/spfx-template-api/src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @packageDocumentation
+ * This package contains the API surface for the SPFx Templating API.
+ */
+
+/**
+ * @public
+ * The name of the SPFx Template API package.
+ */
+export const PACKAGE_NAME: string = '@microsoft/spfx-template-api';
