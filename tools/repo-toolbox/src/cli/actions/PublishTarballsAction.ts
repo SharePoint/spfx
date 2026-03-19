7e00b51c513d2f6e506229bead0f68743377bd73
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2026-03-19 16:50:38 -0700

    Fix an issue with invoking pnpm

--- tools/repo-toolbox/src/cli/actions/PublishTarballsAction.ts
+++ tools/repo-toolbox/src/cli/actions/PublishTarballsAction.ts
@@ -136,7 +136,7 @@ export class PublishTarballsAction extends CommandLineAction {
         try {
           const proc: ChildProcess = Executable.spawn(packageManagerToolFilename, publishArgs, {
             stdio: ['ignore', 'pipe', 'pipe'],
-            environment: { NPM_AUTH_TOKEN: npmToken }
+            environment: { ...process.env, NPM_AUTH_TOKEN: npmToken }
           });
           const { stdout, stderr, exitCode } = await Executable.waitForExitAsync(proc, {
             encoding: 'utf8'
